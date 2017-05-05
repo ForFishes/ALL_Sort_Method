3f27bd68fa7ed12f2a9240c808cb0ba72782addb
ForFishes <2282912238@qq.com>
2017-05-05 11:07:57 +0800

    归并排序

---
+++ MergeSort/MergeSort.cpp
@@ -0,0 +1,37 @@
+// MergeSort.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+// SortTemplate.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+//#include"Sort.h"
+#include<assert.h>
+#include<ctime>
+#include<iostream>
+#include<random>
+using namespace std;
+int main()
+{
+
+	static default_random_engine e(time(0));
+	static uniform_int_distribution<int> u(1, 1000000000);
+	vector<int>vec;
+	for (size_t i = 0; i < 100; i++)
+	{
+		vec.push_back(u(e));
+	}
+
+	//CSort<int>::MergeSort(vec);
+	//assert(CSort<int>::isSorted(vec));
+	//CSort<int>::show(vec);
+	for (auto ele :vec)
+	{
+		cout << ele << endl;
+	}
+
+	return 0;
+}
